13c96c1c86f863de3723fb6105364862db9e5617
dandanBrave <269045975@qq.com>
2017-08-10 11:16:48 +0800

    add exercise2-1

--- javascript数据结构/exercise2-1.js
+++ javascript数据结构/exercise2-1.js
@@ -1,3 +1,28 @@
 /**
  * Created by Administrator on 2017/8/10.
+ * 创建一个记录学生成绩的对象，提供一个添加成绩的方法，以及一个显示学生平均成绩的方法
  */
+function scoreOfStudent() {
+    this.scoreRecord = [];
+    this.add = add;
+    this.average = average;
+}
+function add(grade) {
+    this.scoreRecord.push(grade);
+}
+function average() {
+    var total = 0;
+    for(var i=0;i<this.scoreRecord.length;i++){
+        total += this.scoreRecord[i];
+    }
+    return total/this.scoreRecord.length;
+}
+
+var studentScore = new scoreOfStudent();
+studentScore.add(67);
+studentScore.add(87);
+studentScore.add(76);
+studentScore.add(98);
+studentScore.add(56);
+studentScore.add(91);
+console.log(studentScore.average().toFixed(2));
