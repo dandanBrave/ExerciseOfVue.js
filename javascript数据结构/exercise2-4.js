74213f87c020946026c2c02cc7a17b3e33a99992
dandanBrave <269045975@qq.com>
2017-08-10 12:16:27 +0800

    数据结构与算法javascript描述第二章第四题提交

---
+++ javascript数据结构/exercise2-4.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2017/8/10.
+ * 创建这样一个对象，它将字母存储在一个数组中，并且可以用一个方法将字母连在一起，显示成一个单词
+ */
+function Word(arr) {
+    this.letterArray = arr;
+    this.connect = connect;
+}
+function connect() {
+    return this.letterArray.join("");
+}
+var words = new Word(['h','e','l','l','o']);
+console.log(words.connect());
